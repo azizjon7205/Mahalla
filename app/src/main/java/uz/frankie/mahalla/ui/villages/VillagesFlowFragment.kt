6438504bb18ecdf95cd6d8aa1d49211644226cff
azizj <azizjonsheronov2607@gmail.com>
2023-02-01 21:32:48 +0500

    some changes

--- app/src/main/java/uz/frankie/mahalla/ui/villages/VillagesFlowFragment.kt
+++ app/src/main/java/uz/frankie/mahalla/ui/villages/VillagesFlowFragment.kt
@@ -1,8 +1,17 @@
 package uz.frankie.mahalla.ui.villages
 
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import uz.frankie.mahalla.R
+import uz.frankie.mahalla.databinding.FlowFragmentVillagesBinding
+import uz.frankie.mahalla.ui.BaseFlowFragment
 
-class VillagesFlowFragment : Fragment(R.layout.fragment_villages) {
+class VillagesFlowFragment : BaseFlowFragment(R.layout.flow_fragment_villages, R.id.nav_host_fragment_village) {
+    private val binding by viewBinding(FlowFragmentVillagesBinding::bind)
 
+    override fun setupNavigation(navController: NavController) {
+        super.setupNavigation(navController)
+
+    }
 }
