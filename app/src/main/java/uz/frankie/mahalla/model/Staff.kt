7ac2c40e73bef6062a3bab377a972051f30d6495
Master <Jaxadev>
2023-06-21 14:29:13 +0500

    some changes from  Sarvarbek

---
+++ app/src/main/java/uz/frankie/mahalla/model/Staff.kt
@@ -0,0 +1,14 @@
+package uz.frankie.mahalla.model
+
+data class Staff(
+    val fullname: String,
+    val info:String,
+    val dateOfBirth:String,
+    val province:String,
+    val nationality:String,
+    val degree:String,
+    val major:String,
+    val address:String,
+    val phonenNumber:String,
+    val isMarried:String
+)
--- app/src/main/java/uz/frankie/mahalla/ui/villages/details/StaffInfo.kt
+++ app/src/main/java/uz/frankie/mahalla/ui/villages/details/StaffInfo.kt
@@ -1,4 +1,24 @@
 package uz.frankie.mahalla.ui.villages.details
 
-class StaffInfo {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import uz.frankie.mahalla.R
+import uz.frankie.mahalla.databinding.FragmentStaffInfoBinding
+
+class StaffInfo :Fragment(R.layout.fragment_staff_info){
+    private val binding by viewBinding(FragmentStaffInfoBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initviews()
+    }
+
+    private fun initviews() {
+
+
+
+
+    }
 }
